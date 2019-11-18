71beb54fc2900f390b49917b888189d13cae484e
serk12 <marc.prat.maso@est.fib.upc.edu>
2019-11-18 20:00:20 +0100

    first commit

---
+++ Assets/GoogleARCore/Examples/ObjectManipulation/Scripts/Icons/SelectionPinta.cs
@@ -0,0 +1,83 @@
+//-----------------------------------------------------------------------
+// <copyright file="TranslationManipulator.cs" company="Google">
+//
+// Copyright 2018 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace GoogleARCore.Examples.ObjectManipulation
+{
+    using GoogleARCore.Examples.ObjectManipulationInternal;
+    using UnityEngine;
+
+    /// <summary>
+    /// Manipulates the position of an object via a drag gesture.
+    /// If not selected, the object will be selected when the drag gesture starts.
+    /// </summary>
+    [RequireComponent(typeof(SelectionManipulator))]
+    public class TranslationManipulator : Manipulator
+    {
+
+        /// <summary>
+        /// The Unity's Start method.
+        /// </summary>
+        protected void Start()
+        {
+        }
+
+        /// <summary>
+        /// The Unity's Update method.
+        /// </summary>
+        protected override void Update()
+        {
+        }
+
+        /// <summary>
+        /// Returns true if the manipulation can be started for the given gesture.
+        /// </summary>
+        /// <param name="gesture">The current gesture.</param>
+        /// <returns>True if the manipulation can be started.</returns>
+        protected override bool CanStartManipulationForGesture(DragGesture gesture)
+        {
+            if (gesture.TargetObject == null)
+            {
+                return false;
+            }
+
+            // If the gesture isn't targeting this item, don't start manipulating.
+            if (gesture.TargetObject != gameObject)
+            {
+                return false;
+            }
+
+            // Select it.
+            Select();
+
+            return true;
+        }
+
+        /// <summary>
+        /// Function called when the manipulation is started.
+        /// </summary>
+        /// <param name="gesture">The current gesture.</param>
+        protected override void OnStartManipulation(DragGesture gesture)
+        {
+            Debug.LogError("No instance of ManipulationSystem exists in the scene.");
+        }
+
+
+    }
+}
